99b304b1ce07d8648b8978a30bed111c3bb6c4ee
Sheriff-Hoti <sherifhoti1235@hotmail.com>
2025-05-02 22:27:43 +0200

    Added opts

---
+++ config/opts.nix
@@ -0,0 +1,35 @@
+{
+  opts = {
+    number = true; # Show absolute line numbers
+    relativenumber = true; # Show relative line numbers
+    cursorline = true;
+    cursorlineopt = "number";
+    expandtab = true;
+
+    shiftwidth = 2;
+    smartindent = true;
+    tabstop = 2;
+    softtabstop = 2;
+
+    mouse = "a";
+
+    ignorecase = true;
+    smartcase = true;
+
+    fillchars = {
+      eob = " ";
+    };
+
+    signcolumn = "yes";
+
+    splitbelow = true;
+    splitright = true;
+
+    # timeoutlen = 400;
+
+    # updatetime = 250;
+
+    undofile = true;
+  };
+
+}
