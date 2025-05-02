7f0b94f52128e1572f8874c6089f242ed38406cc
Sheriff-Hoti <sherifhoti1235@hotmail.com>
2025-05-02 22:55:26 +0200

    Fix minor detail

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixvim.url = "github:nix-community/nixvim/nixos-24.11";
+    nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
